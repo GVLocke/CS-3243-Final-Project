c9adb9a1027fb30b56f9b58d8036979a9dbd7837
G Locke <jkeys726@gmail.com>
2024-04-29 21:53:52 -0500

    added skip list

---
+++ FinalProject/SkipList.cs
@@ -0,0 +1,61 @@
+using System.ComponentModel;
+using Maybe.SkipList;
+
+namespace FinalProject;
+
+public class SkipList : IPhoneBook
+{
+    private readonly SkipList<Person> _directory = [];
+
+    public SkipList(StreamReader file)
+    {
+        var lines = new List<string>();
+        using (file)
+        {
+            while (file.ReadLine() is { } line)
+            {
+                lines.Add(line);
+            }
+        }
+        lines.RemoveAt(0);
+        foreach (var person in lines.Select(line => line.Split(',')).Select(parts => new Person(
+                     id: int.Parse(parts[0]),
+                     firstNameLastName: parts[1] + parts[2],
+                     firstName: parts[1],
+                     lastName: parts[2],
+                     address: parts[3],
+                     city: parts[4],
+                     state: parts[5],
+                     zipCode: int.Parse(parts[6]),
+                     email: parts[7],
+                     telephoneNumber: parts[8]
+                 )))
+        {
+            _directory.Add(person);
+        }
+    }
+    public Person? SearchPerson(string firstName, string lastName)
+    {
+        return !_directory.Any() 
+            ? null 
+            : _directory.FirstOrDefault(person => person.FirstNameLastName == firstName + lastName);
+    }
+
+    public void Add(Person person)
+    {
+        _directory.Add(person);
+    }
+
+    public void PrintDirectory()
+    {
+        if (!_directory.Any())
+        {
+            Console.WriteLine("Directory is empty.");
+        }
+
+        foreach (var person in _directory)
+        {
+            Console.WriteLine(person);
+        }
+    }
+}
